cdf57bb74ff991d07e947b0d5e47f8e07b42f507
sfltmya <shanfengliu@imyoon.pro>
2020-01-07 11:33:05 +0800

    feat: v1.0.5

--- bin/model-cli.ts
+++ bin/model-cli.ts
@@ -2,9 +2,10 @@
 
 import * as commander from "commander"
 import { projectCreate } from "../src/index"
+const pg = require(`../../package.json`)
 
 const program = new commander.Command()
-program.version('0.0.1');
+program.version(pg.version);
 
 program
     .option('-n, --new', 'create a new project')
--- src/config/config.ts
+++ src/config/config.ts
@@ -1,9 +1,9 @@
 export const models = {
-  '1': 'npm-base-model',
+  '1': 'npm-base-template',
   '2': 'koa-base-template',
   '3': 'eggjs-base-template',
   '4': 'midway-base-template',
   '5': 'nestjs-base-template',
 }
 
-export const url = 'https://github.com/shanfengliudd01'
+export const url = 'https://github.com/sfltmya'
