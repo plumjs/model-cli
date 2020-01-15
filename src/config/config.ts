fcdd1897c019a2a4d4edb6192e1c5a1924b4a3b4
sfltmya <shanfengliu@imyoon.pro>
2020-01-15 12:35:32 +0800

    feat: add koa-base-template-v2

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,9 +1,10 @@
 export const models = {
   '1': 'npm-base-template',
   '2': 'koa-base-template',
-  '3': 'eggjs-base-template',
-  '4': 'midway-base-template',
-  '5': 'nestjs-base-template',
+  '3': 'koa-base-template-v2',
+  '4': 'eggjs-base-template',
+  '5': 'midway-base-template',
+  '6': 'nestjs-base-template',
 }
 
 export const url = 'https://github.com/sfltmya'
